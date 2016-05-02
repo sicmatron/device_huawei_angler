0f75b98dde36a913515d8735de85865549fd05ff
Daniel Micay <danielmicay@gmail.com>
2016-05-02 17:33:26 -0400

    add boot animation

--- device.mk
+++ device.mk
@@ -160,6 +160,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/huawei/angler/init.angler.qseecomd.sh:system/bin/init.angler.qseecomd.sh
 
+PRODUCT_COPY_FILES += \
+    device/huawei/angler/bootanimation.zip:system/media/bootanimation.zip
+
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 # This device is 560dpi.  However the platform doesn't
