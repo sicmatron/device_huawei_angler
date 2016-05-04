524b27931d3ddbbfc38fa34add87af3af9029deb
Daniel Micay <danielmicay@gmail.com>
2016-05-04 02:05:58 -0400

    switch to in-tree kernel builds

---
+++ AndroidBoard.mk
@@ -0,0 +1,12 @@
+KERNEL_DEFCONFIG := angler_defconfig
+KERNEL_DIR := kernel/huawei/angler
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
