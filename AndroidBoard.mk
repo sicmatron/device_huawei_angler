b1145564efb04d9ae2ad08a3717049459b54121b
Daniel Micay <danielmicay@gmail.com>
2016-09-25 12:08:20 -0400

    split debug and production kernel configuration

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,4 +1,8 @@
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+KERNEL_DEFCONFIG := angler-debug_defconfig
+else
 KERNEL_DEFCONFIG := angler_defconfig
+endif
 KERNEL_DIR := kernel/huawei/angler
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
